5d10723b7169f19c5f303c8e2db5a0d82d5b0f6c
Connor Hennen <connorvhennen@ucla.edu>
2017-03-05 23:47:45 -0800

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -8,7 +8,7 @@ Functions::Functions(QWidget *parent) :
     ui(new Ui::Functions)
 {
     ui->setupUi(this);
-}
+
 
 
 int Functions::getScore(QSpinBox *x) const{
@@ -22,13 +22,14 @@ int Functions::getScore(QSpinBox *x) const{
 
 int Functions::calculateFinalGrade() const{
     double finalGrade = 0;
-    Ui_MainWindow::spinBox -> value;
+    getScore(Ui_MainWindow::spinBox -> value);
     //finalGrade = getScore(MainWindow::ui ->spinBox)
             //*getScore(ui -> MainWindow::spinBox_2)*getScore(ui -> spinBox_3)*getScore(ui -> spinBox_4)*getScore(ui -> spinBox_5)*getScore(ui -> spinBox_6)*getScore(ui -> spinBox_7)*getScore(ui -> spinBox_8)*getScore(ui -> spinBox_9)*getScore(ui -> spinBox_10)*getScore(ui -> spinBox_11);
 
 //    new QLabel(QString::number(finalGrade));
 
 //    MainWindow::connect(finalGrade,SIGNAL(valueChanged(double),ui -> finalScoreValue, SLOT(setValue(double))));
+}
 }
 
 
